efebd8f589ec8a3fdfebbb2dbcce863fbec6eb7c
ouchzsc <ouchzsc@gmail.com>
2018-08-18 12:12:08 +0800

    not imp

--- lua/module/archetype.lua
+++ lua/module/archetype.lua
@@ -4,5 +4,4 @@ archetype.bullet = { mod.GameObject, mod.RenderCircle, mod.DirMove, mod.TimeToLi
 archetype.camera = { mod.WorldCamera, mod.FollowPos, mod.SyncPosition }
 archetype.cursor = { mod.GameObject, mod.RenderCursor, mod.FollowMouse, mod.Trigger }
 archetype.ground = { mod.GameObject, mod.Render, mod.Collider }
-archetype.player = { mod.GameObject, mod.Render, mod.Axis1, mod.Collider, mod.AccSystem, mod.Gravity, mod.Shoot3 }
 return archetype
