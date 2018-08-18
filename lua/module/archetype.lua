8c7bbed679111a75642d359a1c46a40dd0645b36
ouchzsc <ouchzsc@gmail.com>
2018-08-18 12:17:28 +0800

    merge

--- lua/module/archetype.lua
+++ lua/module/archetype.lua
@@ -4,4 +4,6 @@ archetype.bullet = { mod.GameObject, mod.RenderCircle, mod.DirMove, mod.TimeToLi
 archetype.camera = { mod.WorldCamera, mod.FollowTarget, mod.SyncPosition }
 archetype.cursor = { mod.GameObject, mod.RenderCursor, mod.FollowMouse, mod.Trigger }
 archetype.ground = { mod.GameObject, mod.Render, mod.Collider }
+archetype.player = { mod.GameObject, mod.Render, mod.Axis1, mod.Collider, mod.AccSystem, mod.Gravity, mod.WeaponSystem }
+archetype.deadbody = { mod.RenderDeathEffect }
 return archetype
