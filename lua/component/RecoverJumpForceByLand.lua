66597a28dcc1927b2917c2011c4ac418270f5533
ouchzsc <ouchzsc@gmail.com>
2018-08-29 09:33:18 +0800

    Jumpforce RecoverJumpForceByLand.lua

---
+++ lua/component/RecoverJumpForceByLand.lua
@@ -0,0 +1,15 @@
+local RecoverJumpForceByLand = mod.ComponentSystem:newCls()
+
+function RecoverJumpForceByLand:onPopEvent(type, data)
+    local entity = self.entity
+    if type == "onCollision" then
+        local col = data.col
+        if col.type=="slide" and col.normal.y ~= 0 and entity.y < data.other.y then
+            if entity.jumpEnergy< entity.jumpEnergyMax then
+                entity.jumpEnergy = entity.jumpEnergyMax
+            end
+        end
+    end
+end
+
+return RecoverJumpForceByLand
